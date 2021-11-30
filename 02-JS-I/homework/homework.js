feddbd817c4725da5f004b146950bd642d79e618
garciaricar <94265915+garciaricar@users.noreply.github.com>
2021-11-29 21:59:41 -0300

    .

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -165,6 +165,10 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+  var newstr= str + "!"
+  return newstr
+
+  
 }
 
 function combinarNombres(nombre, apellido) {
