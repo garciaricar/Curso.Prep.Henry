7a8a7dd0eddf47a8694ece0bd0fac060a364707b
garciaricar <94265915+garciaricar@users.noreply.github.com>
2021-11-30 14:39:17 -0300

    .

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -199,7 +199,8 @@ function obtenerAreaRectangulo(alto, ancho) {
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
-  
+  var perim = lado*4
+  return perim
 }
 
 
